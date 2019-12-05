8668c7d552a2eda314a278d209db2530ed92316f
Ndumiso Onke Fanti <0nk3.0nk3@gmail.com>
2019-12-05 12:04:31 +0200

    fixing debug levels for log files

--- src/main/java/Validity.java
+++ src/main/java/Validity.java
@@ -10,9 +10,8 @@ public class Validity extends  PasswordChecker{
     private static final Logger logger = LogManager.getLogger(PasswordChecker.class.getName());
 
     public Validity() {
-        System.out.println("********************** Password Checker **********************\n");
+        logger.info("********************** Password Checker **********************");
     }
-
 
     public void passwordIsValid(String password){
 
@@ -29,13 +28,10 @@ public class Validity extends  PasswordChecker{
             logger.error("password should have at least one uppercase letter");
         }
         if(!checkNumber(password)){
-            logger.error("password should at least have one digit");
+            logger.error("password should have at least have one digit");
         }
         if(!checkSpecialCharacter(password)){
             logger.error("password should have at least one special character");
-        }
-        else {
-            logger.info("password is valid");
         }
     }
 
@@ -64,9 +60,10 @@ public class Validity extends  PasswordChecker{
         }
         if(passedConditionsCounter >= minimum){
             logger.debug("User password is ok");
+            System.out.println("User password is ok");
         }else {
             logger.debug("User password is not ok");
-
+            System.out.println("User password not ok");
         }
     }
 
