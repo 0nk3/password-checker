0b552d0707c95cdb38a911c265c7759436ed1bb8
Ndumiso Onke Fanti <0nk3.0nk3@gmail.com>
2019-12-05 13:29:16 +0200

    try catch for passwordIsValid method plus loggin error message generated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
+
+        final Logger logger = LogManager.getLogger(Main.class.getName());
 
         Validity validity = new Validity();
         Scanner input = new Scanner(System.in);
@@ -9,13 +14,17 @@ public class Main {
 
         System.out.println("Please enter a password : ");
         password = input.nextLine();
+        try{
+            validity.passwordIsValid(password);
 
+        }catch(Exception e){
+            logger.error(e.getMessage());
+        }
+        finally {
+            System.out.println("*****************************************************");
+        }
 
-        validity.passwordIsValid(password);
-        System.out.println("*****************************************************");
         validity.passwordIsOk(password);
-
-        System.out.println();
     }
 }
 
