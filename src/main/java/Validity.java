155bcd4b2bef41e7af95fe2cf0f9d03d62299b3f
Ndumiso Onke Fanti <0nk3.0nk3@gmail.com>
2019-12-12 21:11:09 +0200

    changed passwordIsValid to return a boolean rather than void

--- src/main/java/Validity.java
+++ src/main/java/Validity.java
@@ -13,30 +13,43 @@ public class Validity extends  PasswordChecker{
         System.out.println("********************** Password Checker **********************\n");
     }
 
+    /* ********************************************************************* *
+     * Since the maximum number of conditions that must be passed is 6,      *
+     * I will assume 6 as conditions passed, then reduce                     *
+     * it by 1 everyvtime a condition is FAILED.                             *
+     * ********************************************************************* */
+    public boolean passwordIsValid(String password){
 
-    public void passwordIsValid(String password){
+        int passedConditions = 6 ;
 
         if(!passwordExist(password)){
             logger.error("password should exist");
+            passedConditions --;
         }
         if(!passwordLength(password)){
             logger.error("password should be longer than than 8 characters");
+            passedConditions--;
         }
         if(!checkLowerCaseCharacter(password)){
             logger.error("password should have at least one lowercase letter");
+            passedConditions--;
         }
         if(!checkUpperCaseCharacter(password)){
             logger.error("password should have at least one uppercase letter");
+            passedConditions --;
         }
         if(!checkNumber(password)){
             logger.error("password should at least have one digit");
+            passedConditions--;
         }
         if(!checkSpecialCharacter(password)){
             logger.error("password should have at least one special character");
+            passedConditions--;
         }
         else {
             logger.info("password is valid");
         }
+     return (passedConditions >6);
     }
 
     private static final int minimum = 3;   // for the function to return true, a minimum of 3 conditions should be passed
